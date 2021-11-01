b20581442a69463963c8e338092bfc5fd620048e
samukes <darkdarumia@google.com>
2021-11-01 12:51:04 -0300

    WrappingUpTopicController: Adding resources helper to the routes for topic endpoint; Adding delete endpoint at topic; Adding workable behavior on delete form

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -18,10 +18,8 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+
+    resources "/topics", TopicController
   end
 
   if Mix.env() in [:dev, :test] do
